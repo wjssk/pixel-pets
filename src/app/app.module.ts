83bfa3f207b75f54523d91157296d9f549b36a0e
wjssk <psobocinska1@gmail.com>
2023-07-11 18:54:10 +0200

    Starting from scratch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from "./app-component/app.component";
-import { HomePageComponent } from './dashboard/home-page/home-page.component';
+import {HomePageComponent} from './dashboard/home-page/home-page.component';
 import {LoginPageComponent} from "./dashboard/login-page/login-page.component";
-import { SignUpComponent } from './dashboard/sign-up/sign-up.component';
-import { BasicButtonComponent } from './shared/components/basic-button/basic-button.component';
-import { StatusBarComponent } from './shared/components/status-bar/status-bar.component';
-import { WalkComponent } from './dashboard/walk/walk.component';
+import {SignUpComponent} from './dashboard/sign-up/sign-up.component';
+import {BasicButtonComponent} from './shared/components/basic-button/basic-button.component';
+import {StatusBarComponent} from './shared/components/status-bar/status-bar.component';
+import {WalkComponent} from './dashboard/walk/walk.component';
 
 @NgModule({
   declarations: [
@@ -22,9 +22,10 @@ import { WalkComponent } from './dashboard/walk/walk.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/dashboard/walk/walk.component.ts
+++ src/app/dashboard/walk/walk.component.ts
@@ -1,50 +1,5 @@
-import { Component } from '@angular/core';
-import Phaser from 'phaser';
-
-class MyScene extends Phaser.Scene {
-  private player!: Phaser.Physics.Arcade.Sprite;
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
-  private map!: Phaser.GameObjects.TileSprite;
-
-  constructor() {
-    super('MyScene');
-  }
-
-  preload() {
-    this.load.image('map', 'assets/images/laka.jpg');
-    this.load.image('player', 'assets/images/postac.jpg');
-  }
-
-  create() {
-    this.map = this.add.tileSprite(0, 0, 2000, 2000, 'map');
-    this.cameras.main.setBounds(0, 0, this.map.width, this.map.height);
-    this.cameras.main.centerOn(0, 0);
-
-    this.player = this.physics.add.sprite(0, 0, 'player');
-    this.cameras.main.startFollow(this.player, true);
-
-    this.cursors = this.input!.keyboard!.createCursorKeys();
-  }
-
-  override update() {
-    const speed = 200;
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-speed);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(speed);
-    } else {
-      this.player.setVelocityX(0);
-    }
-
-    if (this.cursors.up.isDown) {
-      this.player.setVelocityY(-speed);
-    } else if (this.cursors.down.isDown) {
-      this.player.setVelocityY(speed);
-    } else {
-      this.player.setVelocityY(0);
-    }
-  }
-}
+import { Component, ViewChild, ElementRef, AfterViewInit, HostListener } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-walk',
@@ -52,27 +7,4 @@ class MyScene extends Phaser.Scene {
   styleUrls: ['./walk.component.scss']
 })
 export class WalkComponent {
-  private game!: Phaser.Game;
-
-  ngOnInit(): void {
-    const config: Phaser.Types.Core.GameConfig = {
-      type: Phaser.AUTO,
-      width: window.innerWidth,
-      height: window.innerHeight,
-      parent: 'gameContainer',
-      physics: {
-        default: 'arcade',
-        arcade: {
-          gravity: { y: 0 }
-        }
-      },
-      scene: MyScene
-    };
-
-    this.game = new Phaser.Game(config);
-  }
-
-  ngOnDestroy(): void {
-    this.game.destroy(true);
-  }
 }
