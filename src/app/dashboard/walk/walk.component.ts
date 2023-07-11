d648792d5d65e557dcef9344651410a2c47cd482
wjssk <psobocinska1@gmail.com>
2023-07-11 06:18:38 +0200

    Test

--- src/app/dashboard/walk/walk.component.ts
+++ src/app/dashboard/walk/walk.component.ts
@@ -1,88 +1,76 @@
-import { Component, HostListener, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import Phaser from 'phaser';
 @Component({
   selector: 'app-walk',
   templateUrl: './walk.component.html',
   styleUrls: ['./walk.component.scss']
 })
-export class WalkComponent implements AfterViewInit {
-  @ViewChild('mapContainer') mapContainer!: ElementRef;
-  mapSource: string = 'assets/images/laka.jpg';
-  userSource: string = 'assets/images/postac.jpg';
-  userPositionX: number = 0;
-  userPositionY: number = 0;
-  userSpeed: number = 10; // Adjust the user's movement speed as desired
-  boundaryMargin: number = 10; // Adjust the margin from the screen edge at which the map scrolls
-  mapWidth: number = 2000; // Width of the map image
-  mapHeight: number = 2000; // Height of the map image
+export class WalkComponent {
+  private game!: Phaser.Game;
+  private player!: Phaser.Physics.Arcade.Sprite;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private map!: Phaser.GameObjects.TileSprite;
 
-  ngAfterViewInit(): void {
-    this.centerMap();
+  constructor() {
+  }
+  ngOnInit(): void {
+    const config: Phaser.Types.Core.GameConfig = {
+      type: Phaser.AUTO,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      parent: 'gameContainer',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 0 }
+        }
+      },
+      scene: {
+        preload: this.preload,
+        create: this.create,
+        update: this.update
+      }
+    };
+
+    this.game = new Phaser.Game(config);
   }
 
-  centerMap(): void {
-    const mapContainer = this.mapContainer.nativeElement;
-    const containerWidth = mapContainer.clientWidth;
-    const containerHeight = mapContainer.clientHeight;
-    const maxScrollLeft = this.mapWidth - containerWidth;
-    const maxScrollTop = this.mapHeight - containerHeight;
-
-    mapContainer.scrollLeft = (maxScrollLeft - containerWidth) / 2;
-    mapContainer.scrollTop = (maxScrollTop - containerHeight) / 2;
+  preload(): void {
+    this.load.image('map', 'assets/images/laka.jpg');
+    this.load.image('player', 'assets/images/postac.jpg');
   }
 
-  @HostListener('window:keydown', ['$event'])
-  handleKeyPress(event: KeyboardEvent) {
-    const { key } = event;
+  create(): void {
+    this.map = this.add.tileSprite(0, 0, 2000, 2000, 'map');
+    this.cameras.main.setBounds(0, 0, this.map.width, this.map.height);
+    this.cameras.main.centerOn(0, 0);
 
-    if (key === 'ArrowUp' || key === 'w') {
-      this.moveUser(0, -this.userSpeed);
-    } else if (key === 'ArrowDown' || key === 's') {
-      this.moveUser(0, this.userSpeed);
-    } else if (key === 'ArrowLeft' || key === 'a') {
-      this.moveUser(-this.userSpeed, 0);
-    } else if (key === 'ArrowRight' || key === 'd') {
-      this.moveUser(this.userSpeed, 0);
+    this.player = this.physics.add.sprite(0, 0, 'player');
+    this.cameras.main.startFollow(this.player, true);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update(): void {
+    const speed = 200;
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-speed);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(speed);
+    } else {
+      this.player.setVelocityX(0);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-speed);
+    } else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(speed);
+    } else {
+      this.player.setVelocityY(0);
     }
   }
 
-  moveUser(deltaX: number, deltaY: number): void {
-    const mapContainer = this.mapContainer.nativeElement;
-    const containerWidth = mapContainer.clientWidth;
-    const containerHeight = mapContainer.clientHeight;
-    const scrollLeft = mapContainer.scrollLeft;
-    const scrollTop = mapContainer.scrollTop;
-    const maxScrollLeft = this.mapWidth - containerWidth;
-    const maxScrollTop = this.mapHeight - containerHeight;
-
-    // Calculate new positions
-    let newUserPositionX = this.userPositionX + deltaX;
-    let newUserPositionY = this.userPositionY + deltaY;
-    let newScrollLeft = scrollLeft;
-    let newScrollTop = scrollTop;
-
-    // Check if we're close to the boundary, and if so, scroll the map
-    if (newUserPositionX < this.boundaryMargin && scrollLeft > 0) {
-      newScrollLeft = Math.max(0, scrollLeft - this.userSpeed);
-      newUserPositionX = this.userPositionX;
-    } else if (newUserPositionX > containerWidth - this.boundaryMargin && scrollLeft < maxScrollLeft) {
-      newScrollLeft = Math.min(maxScrollLeft, scrollLeft + this.userSpeed);
-      newUserPositionX = this.userPositionX;
-    }
-
-    if (newUserPositionY < this.boundaryMargin && scrollTop > 0) {
-      newScrollTop = Math.max(0, scrollTop - this.userSpeed);
-      newUserPositionY = this.userPositionY;
-    } else if (newUserPositionY > containerHeight - this.boundaryMargin && scrollTop < maxScrollTop) {
-      newScrollTop = Math.min(maxScrollTop, scrollTop + this.userSpeed);
-      newUserPositionY = this.userPositionY;
-    }
-
-    // Apply the new positions
-    this.userPositionX = newUserPositionX;
-    this.userPositionY = newUserPositionY;
-    mapContainer.scrollLeft = newScrollLeft;
-    mapContainer.scrollTop = newScrollTop;
+  ngOnDestroy(): void {
+    this.game.destroy(true);
   }
-
 }
